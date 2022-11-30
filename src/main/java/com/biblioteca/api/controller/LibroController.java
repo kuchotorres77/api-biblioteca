f416a447c25882877ed8cc3559406845d9ab21b9
kuchotorres77 <kuchotorres.77@gmail.com>
2022-11-30 01:17:33 -0300

    cors

--- src/main/java/com/biblioteca/api/controller/LibroController.java
+++ src/main/java/com/biblioteca/api/controller/LibroController.java
@@ -20,7 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://appfrontbiblioteca.web.app/")
 public class LibroController {
 
     @Autowired
